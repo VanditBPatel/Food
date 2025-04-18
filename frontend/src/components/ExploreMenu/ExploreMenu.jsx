bdc84fb4e30f8335ff27a7cece495afc6bc649e7
VanditBPatel <vbpatelfbgame@gmail.com>
2025-04-18 12:18:48 +0530

    Add assets, components, and pages for the food ordering application
    - Added various image assets including profile icon, rating stars, and icons for navigation.
    - Implemented ExploreMenu component with styling and functionality to display menu items.
    - Created Header component with a background image and introductory text.
    - Developed Navbar component for navigation with active state management.
    - Set up Cart and PlaceOrder pages with basic structure.
    - Established Home page to integrate Header and ExploreMenu components.
    - Configured global styles and Vite setup for the project.

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+import { preview } from 'vite'
+
+const ExploreMenu({category,setcategory}) => {
+
+    return (
+        <div className='explore-menu' id='explore-menu'>
+            <h1>Explore our menu</h1>
+            <p className='explore-menu-text' id='explore-menu'>
+                Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.
+            </p>
+            <div className="explore-menu-list">
+                {menu_list.map((item, index) => {
+                    return (
+                        <div onClick={()=>setcategory(prev=>prev===item.menu_name?"All")} key={index} className='explore-menu-list-item'>
+                            <img src={item.menu_image} alt="" />
+                            <p>{item.menu_name}</p>
+                        </div>  
+                    )
+                })}
+            </div>
+            <hr />
+        </div>
+    )
+}
+
+export default ExploreMenu
