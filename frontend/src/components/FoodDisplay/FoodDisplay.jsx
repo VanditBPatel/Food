ae31bfc20741eaaaf70d46cc5f656a62e715ed0c
VanditBPatel <vbpatelfbgame@gmail.com>
2025-04-19 23:49:13 +0530

    Refactor FoodDisplay and FoodItem components to integrate cart functionality and improve item rendering logic

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,8 +10,10 @@ const FoodDisplay = ({category}) => {
   return (
     <div className='food-display' id='food-display'>
         <h2>Top dishes near you</h2>
-        < className="food-display-list">
+        <div className="food-display-list">
             {food_list.map((item, index) => {
+              {console.log(item.category,item.category);}
+                if (category==="All" || category===item.category) return null
                 return <FoodItem key={index} id={item._id} name={item.name} price={item.price} description={item.description} price={item.price} image={item.image}/>
             })}
         </div>
--- frontend/src/context/Storecontext.jsx
+++ frontend/src/context/Storecontext.jsx
@@ -1,14 +1,36 @@
-import { createContext } from "react";
+import { createContext, use, useEffect } from "react";
 import { food_list } from "../assets/assets";
 
 export const storeContext = createContext(null)
 
 const StoreContextProvider = () => {
 
+    const [cartItems, setCartItems] = useState({});
+
+    const addTocart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+        } 
+        else{
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => {...prev, [itemId]: prev[itemId] - 1 }))
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
 
 
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addTocart,
+        removeFromCart,
     }
     return(
         <storeContext.Provider value={contextValue}>
